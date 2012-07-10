6841222f30705111896e1ea32fba6363264b5f72
Hubert Boma Manilla <b4bomsy@gmail.com>
2012-07-10 12:03:26 -0400

    updates

--- d3digilights.js
+++ d3digilights.js
@@ -4,101 +4,147 @@ function DigiLights(config){
     this.init();
 }
 DigiLights.version = "1.0.0";
-DigiLights.charset = {
-    dimensions:[6,7],
-    matrix: {
-        "0": [
-            1,1,1,1,1,0,
-            1,0,0,0,1,0,
-            1,0,0,0,1,0,
-            1,0,0,0,1,0,
-            1,0,0,0,1,0,
-            1,0,0,0,1,0,
-            1,1,1,1,1,0
-            ],
-        "1": [
-            0,0,0,1,0,0,
-            0,0,0,1,0,0,
-            0,0,0,1,0,0,
-            0,0,0,1,0,0,
-            0,0,0,1,0,0,
-            0,0,0,1,0,0,
-            0,0,0,1,0,0
-            ],
-        "2": [
-            1,1,1,1,1,0,
-            0,0,0,0,1,0,
-            0,0,0,0,1,0,
-            1,1,1,1,1,0,
-            1,0,0,0,0,0,
-            1,0,0,0,0,0,
-            1,1,1,1,1,0
-            ],
-        "3": [
-            1,1,1,1,1,0,
-            0,0,0,0,1,0,
-            0,0,0,0,1,0,
-            1,1,1,1,1,0,
-            0,0,0,0,1,0,
-            0,0,0,0,1,0,
-            1,1,1,1,1,0
-            ],
-        "4": [
-            1,0,0,0,1,0,
-            1,0,0,0,1,0,
-            1,0,0,0,1,0,
-            1,1,1,1,1,0,
-            0,0,0,0,1,0,
-            0,0,0,0,1,0,
-            0,0,0,0,1,0
-            ],
-        "5": [
-            1,1,1,1,1,0,
-            1,0,0,0,0,0,
-            1,0,0,0,0,0,
-            1,1,1,1,1,0,
-            0,0,0,0,1,0,
-            0,0,0,0,1,0,
-            1,1,1,1,1,0
-            ],
-        "6": [
-            1,1,1,1,1,0,
-            1,0,0,0,0,0,
-            1,0,0,0,0,0,
-            1,1,1,1,1,0,
-            1,0,0,0,1,0,
-            1,0,0,0,1,0,
-            1,1,1,1,1,0
-            ],
-        "7": [
-            1,1,1,1,1,0,
-            0,0,0,0,1,0,
-            0,0,0,0,1,0,
-            0,0,0,0,1,0,
-            0,0,0,0,1,0,
-            0,0,0,0,1,0,
-            0,0,0,0,1,0
-            ],
-        "8": [
-            1,1,1,1,1,0,
-            1,0,0,0,1,0,
-            1,0,0,0,1,0,
-            1,1,1,1,1,0,
-            1,0,0,0,1,0,
-            1,0,0,0,1,0,
-            1,1,1,1,1,0
-            ],
-        "9": [
-            1,1,1,1,1,0,
-            1,0,0,0,1,0,
-            1,0,0,0,1,0,
-            1,1,1,1,1,0,
-            0,0,0,0,1,0,
-            0,0,0,0,1,0,
-            1,1,1,1,1,0
-        ]
-    }
-    
+DigiLights.characterMatrix = {
+        "0": {
+            "x": 5,
+            "y": 7,
+            "matrix":[
+                1,1,1,1,1,
+                1,0,0,0,1,
+                1,0,0,0,1,
+                1,0,0,0,1,
+                1,0,0,0,1,
+                1,0,0,0,1,
+                1,1,1,1,1
+            ]
+        },
+        "1": {
+            "x": 5,
+            "y": 7,
+            "matrix":[
+                0,0,0,1,0,
+                0,0,0,1,0,
+                0,0,0,1,0,
+                0,0,0,1,0,
+                0,0,0,1,0,
+                0,0,0,1,0,
+                0,0,0,1,0
+            ]
+        },
+        "2": {
+            "x": 5,
+            "y": 7,
+            "matrix":[
+                1,1,1,1,1,
+                0,0,0,0,1,
+                0,0,0,0,1,
+                1,1,1,1,1,
+                1,0,0,0,0,
+                1,0,0,0,0,
+                1,1,1,1,1
+            ]
+        },
+        "3": {
+            "x": 5,
+            "y": 7,
+            "matrix":[
+                1,1,1,1,1,
+                0,0,0,0,1,
+                0,0,0,0,1,
+                1,1,1,1,1,
+                0,0,0,0,1,
+                0,0,0,0,1,
+                1,1,1,1,1
+            ]
+        },
+         "4": {
+            "x": 5,
+            "y": 7,
+            "matrix":[
+                1,0,0,0,1,
+                1,0,0,0,1,
+                1,0,0,0,1,
+                1,1,1,1,1,
+                0,0,0,0,1,
+                0,0,0,0,1,
+                0,0,0,0,1
+            ]
+        },
+        "5": {
+            "x": 5,
+            "y": 7,
+            "matrix":[
+                1,1,1,1,1,
+                1,0,0,0,0,
+                1,0,0,0,0,
+                1,1,1,1,1,
+                0,0,0,0,1,
+                0,0,0,0,1,
+                1,1,1,1,1
+            ]
+        },
+        "6": {
+            "x": 5,
+            "y": 7,
+            "matrix":[
+                1,1,1,1,1,
+                1,0,0,0,0,
+                1,0,0,0,0,
+                1,1,1,1,1,
+                1,0,0,0,1,
+                1,0,0,0,1,
+                1,1,1,1,1
+            ]
+        },
+        "7": {
+            "x": 5,
+            "y": 7,
+            "matrix":[
+                1,1,1,1,1,
+                0,0,0,0,1,
+                0,0,0,0,1,
+                0,0,0,0,1,
+                0,0,0,0,1,
+                0,0,0,0,1,
+                0,0,0,0,1
+            ]
+        },
+        "8": {
+            "x": 5,
+            "y": 7,
+            "matrix":[
+                1,1,1,1,1,
+                1,0,0,0,1,
+                1,0,0,0,1,
+                1,1,1,1,1,
+                1,0,0,0,1,
+                1,0,0,0,1,
+                1,1,1,1,1
+            ]
+        },
+        "9": {
+            "x": 5,
+            "y": 7,
+            "matrix":[
+                1,1,1,1,1,
+                1,0,0,0,1,
+                1,0,0,0,1,
+                1,1,1,1,1,
+                0,0,0,0,1,
+                0,0,0,0,1,
+                1,1,1,1,1
+            ]
+        },
+        "blnk":{
+           "x":1,
+           "y":7,
+           "matrix":[ 0,0,0,0,0,0,0 ]
+        },
+        ":":{
+            "x":1,
+            "y":7,
+            "matrix":[ 0,0,1,0,1,0,0 ]
+        }
 }
 DigiLights.prototype = {
     
@@ -119,46 +165,83 @@ DigiLights.prototype = {
         }
     },
     setText:function(string){
-        if(typeof string === "string" ){
-            this.showText = string;
+        if(typeof string !== "undefined" ){
+            this.text = string;
         }else{
             throw Error("setText: Value is not a string");
         }
     },
     on:function(){
-        var that = this;
+        var that = this,
+            nxtPos = null,
+            blank = "blnk",
+            blankObject = this.getCharMatrix(blank);
         this.getCharGroupMatrix(this.text,
-                function(c,m,i){
-                    that.drawMatrix({
+                function(c,co,i){
+
+                    nxtPos = that.drawMatrix({
                         color:"#f00",
                         container: that.viewer.canvas,
-                        data: m,
-                        position:{x:i*72,y:0}
+                        data: co.matrix,
+                        x_size: co.x,
+                        y_size: co.y,
+                        position: nxtPos,
+                        index: i,
+                        text: c
                     });
-                    console.log(m);
+                    //draw blank line
+                    nxtPos = that.drawMatrix({
+                        color:"#f00",
+                        container: that.viewer.canvas,
+                        data: blankObject.matrix,
+                        x_size: blankObject.x,
+                        y_size: blankObject.y,
+                        position: nxtPos,
+                        index: blank+i,
+                        text: blank
+                    });
             });
     },
     drawMatrix:function(args){
-        var dim = DigiLights.charset.dimensions,
-            x_size = dim[0],
-            y_size = dim[1],
+        var x_size = args.x_size,
+            y_size = args.y_size,
+            ref = args.container,
             cx = 0,
             cy = 10,
             INC =10,
             RADIUS = 5,
             SPACE = 2,
-            pos = args.position;
-        args.container.append("g")
-            .attr("transform","translate("+pos.x+","+pos.y+")")
-            .selectAll("circle")
-            .data(args.data)
-        .enter().append("circle")
-            .attr("cx",function(d,i){ if(i%x_size === 0){cx = 0;} cx +=(INC + SPACE); return cx;})
+            g_width = 0,
+            G = "grp",
+            pos = args.position === null? {x:0,y:0}: args.position,
+            group = null;
+            group = ref.select("#"+G+args.index);
+            
+        if(group[0][0] === null){
+            group = ref.append("g")
+                    .attr("id",G+args.index)
+                    .attr("transform","translate("+pos.x+","+pos.y+")") 
+        }  
+        group = group.selectAll("circle")
+                .data(args.data)
+            
+        //new
+        group.enter().append("circle")
+            .attr("cx",function(d,i){ if(i%x_size === 0){ if(g_width === 0){g_width = cx;} cx = 0; } cx +=(INC + SPACE); return cx;})
             .attr("cy",function(d,i){if(i%x_size === 0){ cy += (INC + SPACE); } return cy;})
             .attr("stroke",args.color)
             .attr('fill',function(d){return d === 1 ? args.color :'none'})
             .attr("r",RADIUS)
+        //update
+        group.attr('fill',function(d){return d === 1 ? args.color :'none'});
         
+        //remove
+        group.exit().remove();
+            //return new position coordinates for the next drawing
+        return {
+            x:pos.x + g_width,
+            y:pos.y
+        }
     },
     setupViewer: function(container){
         var cObject = document.getElementById(container),
@@ -177,22 +260,22 @@ DigiLights.prototype = {
            h:canvas_height
         };
     },
-    
+    //returns the character object containing the matrix
     getCharMatrix: function(char){
-        return DigiLights.charset.matrix[char];
+        return DigiLights.characterMatrix[char];
     },
     
     getCharGroupMatrix:function(charString, callback){
         var group = charString.split(""),
             charMatrixSet = [],
-            matrix = null,
+            charObject = null,
             that = this;     
         group.forEach(function(char, index){
-            matrix = that.getCharMatrix(char);
+            charObject = that.getCharMatrix(char);
             if(typeof callback === "function"){
-                callback(char,matrix,index);
+                callback(char,charObject,index);
             }
-            charMatrixSet.push(matrix);
+            charMatrixSet.push(charObject.matrix);
         });
         return group;
     }
