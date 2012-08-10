4a4c0625ee8749cfc368dea09580e9a53a68838d
Hubert Boma Manilla <b4bomsy@gmail.com>
2012-08-10 08:34:52 -0400

    restructure dlite.js, and second sample

--- dlite.js
+++ dlite.js
@@ -4,14 +4,16 @@ function Dlite(ref) {
         return new Dlite(ref);
     }
     this.text = null;
-    this.ref = null;
+    this.ref = "#" + ref;
     this.size = null;
     this.color = null;
     this.align = null;
     this.width = null;
     this.height = null;
     this.showBg = null;
-    this.init(ref);
+    this.viewer = {};
+
+    this._initialize();
 }
 
 Dlite.version = "1.0.0";
@@ -19,6 +21,12 @@ Dlite.version = "1.0.0";
 Dlite.align = {
     VERTICAL: 0,
     HORIZONTAL: 1
+};
+
+Dlite.background = {
+    SHOW: 2,
+    PARTIAL: 1,
+    NONE: 0
 };
 
 Dlite.waveforms = {
@@ -182,7 +190,7 @@ Dlite.characterMatrix = {
             1, 1, 1, 1, 1
         ]
     },
-    "_": {
+    "_s": {
         "x": 1,
         "y": 7,
         "matrix": [ 0, 0, 0, 0, 0, 0, 0 ]
@@ -249,56 +257,161 @@ Dlite.characterMatrix = {
     }
     
 }
-Dlite.prototype = {  
-    viewer: {
-        canvas: null,
-        width: 0,
-        height: 0
-    },
-    
-    init: function(elem){
+Dlite.prototype = {   
+   _initialize: function(){
         var v = this.viewer,
         o = null;
-        //setup animation frame shim for the browser
-        /*window.requestAnimFrame = window.requestAnimFrame || (function(){
-                return window.requestAnimationFrame        ||
-                       window.webkitRequestAnimationFrame  ||
-                       window.mozRequestAnimationFrame     ||
-                       window.oRequestAnimationFrame       ||
-                       window.msRequestAnimationFrame      ||
-                       function(callback){
-                           window.setTimeout(callback, 1000/60);
-                       };
-                       
-            })();*/
-        this._setConfiguration(elem);
+        this._setDefaultConfiguration();
         //setup the svg canvas
-        if(v.canvas === null){
-            o = this.setupViewer(this.ref);
+        if(!v.canvas){
+            o = this._setupSVGViewer(this.ref);
             v.canvas = o.c;
             v.width = o.w;
             v.height = o.h;
         }
+    }, 
+    
+    _setDefaultConfiguration: function(){
+         //setting up default configuration 
+        this.setSize(5);
+        this.setColor("#f00");
+        this.setBackgroundColor("#000");
+        this.showBackground(Dlite.background.SHOW);
+        this.setAlignment(Dlite.align.HORIZONTAL);
     },
     
-    _setConfiguration: function(ref){
-        var SIZE = 5,
-            COLOR = "#f00",
-            BGCOLOR = "#000",
-            BGDISPLAY = true,
-            ALIGN = Dlite.align.HORIZONTAL,
-            refElement = ref;
-             /* setting up configuration */ 
-            this.setReference(refElement)
-            this.setSize(SIZE);
-            this.setColor(COLOR);
-            this.setBackgroundColor(BGCOLOR);
-            this.showBackground(BGDISPLAY);
-            this.setAlignment(ALIGN);
+    _setupSVGViewer: function(container){
+        var cObject = document.querySelector(container),
+        style = window.getComputedStyle(cObject, null),
+        canvas_width = style.getPropertyValue("width"),
+        canvas_height = style.getPropertyValue("height"),
+        svg = d3.select(container).append("svg")
+            .attr('width',canvas_width)
+            .attr('height',canvas_height)
+            .append("g")
+            .attr("transform","translate(0,0)");
+        
+        return {
+            c:svg,
+            w:canvas_width,
+            h:canvas_height
+        };
     },
     
+    _drawSVG:function(args){
+        var x_size = args.x_size,
+            y_size = args.y_size,
+            ref = args.container,
+            align = args.align,
+            cx = 0,
+            cy = 10,
+            q = this.showBg,
+            INC = args.radius * 2,
+            RADIUS = args.radius,
+            SPACE = args.radius / 2,
+            g_width = 0,
+            g_height = 0,
+            G = "grp",
+            pos = args.position === null? {x:0,y:0} : args.position,
+            group = null,
+            id = G+args.index;
+            group = ref.select("#"+id);
+            
+            if(group[0][0] === null){
+                group = ref.append("g")
+                .attr("id", id)
+                .attr("transform","translate("+pos.x+","+pos.y+")"); 
+            }  
+            group = group.selectAll("circle")
+                .data(args.data)
+            
+            //new
+            group.enter().append("circle")
+                .attr("cx",function(d, i){ if(i%x_size === 0){ if(g_width === 0 && align === Dlite.align.HORIZONTAL){g_width = cx;} cx = 0; } cx += (INC + SPACE); return cx;})
+                .attr("cy",function(d, i){ if(i%x_size === 0){ cy += (INC + SPACE); } if(align === Dlite.align.VERTICAL){ g_height = cy;} return cy; })
+                .attr("stroke", function(d){if((q === Dlite.background.SHOW || q === Dlite.background.PARTIAL) || d === 1){ return args.color; }else{return "none";}})
+                .attr("stroke-opacity",function(d){return q === Dlite.background.SHOW ? "0.2" : "0.5";})
+                .attr("fill", function(d){return q === Dlite.background.SHOW || d === 1 ? args.color :"none"})
+                .attr("fill-opacity",function(d){return q === Dlite.background.SHOW && d !== 1 ? "0.2" : "1.0";})
+                .attr("r", RADIUS);
+                
+            //update
+            group.attr("stroke",function(d){if((q === Dlite.background.SHOW || q === Dlite.background.PARTIAL) || d === 1){ return args.color; }else{return "none";}})
+                .attr("stroke-opacity",function(d){return q === Dlite.background.SHOW ? "0.2" : "0.5";})
+                .attr("fill",function(d){return q === Dlite.background.SHOW || d === 1 ? args.color :"none"})
+                .attr("fill-opacity",function(d){return q === Dlite.background.SHOW && d !== 1 ? "0.2" : "1.0"});
+            
+            //remove
+            group.exit().remove();
+            //return new position coordinates for the next drawing
+            return {
+                x:pos.x + g_width,
+                y:pos.y + g_height
+            }
+    },
+    
+    _getFormattedContent: function(content){
+        var toString  = Object.prototype.toString,
+            rContent = null;
+        if(typeof content === "string"){
+            rContent = content.split("");
+        }else if (typeof content === "number"){
+            rContent = String(content);
+            if(rContent !== null || typeof rContent !== "undefined"){
+                rContent = rContent.split("");
+            }
+        }else if (toString.call(content) === "[object Array]"){
+            rContent = content;
+        }
+        //add spaces
+        return rContent;
+    },
+    
+    _renderContent: function(content){
+        var fContent = null,
+            that = this,
+            nextPosition = null;
+        fContent = this._getFormattedContent(content);
+        if(fContent !== null){
+           this._getArrayMatrix(fContent, 
+               function(content, contentMatrix, index ){
+                   var config = {
+                       color: that.color,
+                       index: index,
+                       text: content,
+                       x_size: contentMatrix.x,
+                       y_size: contentMatrix.y,
+                       container: that.viewer.canvas,
+                       align: that.align,
+                       radius: that.size,
+                       position: nextPosition,
+                       data: contentMatrix.matrix
+                   }
+                   nextPosition = that._drawSVG(config);
+                });
+        }
+        
+    },
+
+    _getMatrix: function(character){
+        return Dlite.characterMatrix[character];
+    },
+    
+    _addSpace: function(ommit, character, index, callback){
+        var space = "_s";
+        if(character.indexOf(ommit) === -1){
+            callback(space, this._getMatrix(space),"s"+ index);
+        }
+    },
+    
+    _getArrayMatrix: function(contentArray, callback){
+        for(var i = 0; i < contentArray.length; i+=1){
+            callback(contentArray[i], this._getMatrix(contentArray[i]), i);
+            this._addSpace("$", contentArray[i], i, callback);
+        }
+    },
+      
     setContent: function(content){
-        console.log(content);
         var toString = Object.prototype.toString;
         if(typeof content !== "undefined" || typeof content === "string" || typeof content === "number" || toString.call(content) === "[object Array]" ){
             this.text = content;
@@ -326,16 +439,6 @@ Dlite.prototype = {
        }
        return this;
     },
-    
-    setReference: function(ref){
-        if(typeof ref !== "undefined" ){
-            this.ref = ref;
-        }else{
-            throw Error("setReference: Value is not valid");
-        }
-        return this;
-    },
-    
     setAlignment: function(alignment){
         if(alignment === Dlite.align.HORIZONTAL || alignment === Dlite.align.VERTICAL){
             this.align = alignment;
@@ -355,8 +458,11 @@ Dlite.prototype = {
        return this;
     },
     
-    showBackground: function(state){
-        this.showBg = state === false ? false : true;
+    showBackground: function(state){      
+        if(state !== Dlite.background.NONE && state !== Dlite.background.PARTIAL && state !== Dlite.background.SHOW ){
+            throw Error("setBackground: state is invalid")
+        }
+        this.showBg = state;
         return this;
     },
     
@@ -369,22 +475,23 @@ Dlite.prototype = {
             w = window,
             interv = null,
             that = this,
-            action = function(callback){
-                interv = w.setInterval(function(){ 
-                    if(counter < duration || duration === null){                            
-                        callback(that); 
-                        that._renderContent(that.text);
-                        if(duration !== null){
-                            counter+=1;
-                        }
-                    }else{
-                        if(interv !== null){
-                            w.clearInterval(interv);
-                        }
+            action;
+        action = function(callback){
+            interv = w.setInterval(function(){ 
+                if(counter < duration || duration === null){                            
+                    callback(that); 
+                    that._renderContent(that.text);
+                    if(duration !== null){
+                        counter+=1;
                     }
-                }, interval);
-            };
-            
+                }else{
+                    if(interv !== null){
+                        w.clearInterval(interv);
+                    }
+                }
+            }, interval);
+        };
+        
         if(args.length > 0){
             if(typeof args[0] ==="number"){
                 delay = args[0]; //start
@@ -401,124 +508,5 @@ Dlite.prototype = {
         }else{
             this._renderContent(this.text);
         }
-    },
-    
-     _getFormattedContent: function(content){
-        var toString  = Object.prototype.toString,
-            rContent = null;
-        if(typeof content === "string"){
-            rContent = content.split("");
-        }else if (typeof content === "number"){
-            rContent = String(content);
-            if(rContent !== null || typeof rContent !== "undefined"){
-                rContent = rContent.split("");
-            }
-        }else if (toString.call(content) === "[object Array]"){
-            rContent = content;
-        }
-        //add spaces
-        return rContent;
-    },
-    
-    _renderContent: function(content){
-        var fContent = null,
-            that = this,
-            nextPosition = null;
-        fContent = this._getFormattedContent(content);
-        if(fContent !== null){
-           this._getArrayMatrix(fContent, 
-               function(content, contentMatrix, index ){
-                   console.log(content);
-                   var config = {
-                       color: that.color,
-                       index: index,
-                       text: content,
-                       x_size: contentMatrix.x,
-                       y_size: contentMatrix.y,
-                       container: that.viewer.canvas,
-                       align: that.align,
-                       radius: that.size,
-                       position: nextPosition,
-                       data: contentMatrix.matrix
-                   }
-                   nextPosition = that._drawSVG(config);
-                });
-        }
-        
-    },
-    
-     //returns the character object containing the matrix
-    _getMatrix: function(character){
-        return Dlite.characterMatrix[character];
-    },
-    _getArrayMatrix: function(contentArray, callback){
-        for(var i = 0; i < contentArray.length; i+=1){
-                callback(contentArray[i], this._getMatrix(contentArray[i]), i);
-        }
-    },
-    
-    _drawSVG:function(args){
-        var x_size = args.x_size,
-            y_size = args.y_size,
-            ref = args.container,
-            align = args.align,
-            cx = 0,
-            cy = 10,
-            q = this.showBg,
-            INC = args.radius * 2,
-            RADIUS = args.radius,
-            SPACE = args.radius / 2,
-            g_width = 0,
-            g_height = 0,
-            G = "grp",
-            pos = args.position === null? {x:0,y:0} : args.position,
-            group = null;
-            group = ref.select("#"+G+args.index);
-            
-            if(group[0][0] === null){
-                group = ref.append("g")
-                .attr("id",G+args.index)
-                .attr("transform","translate("+pos.x+","+pos.y+")"); 
-            }  
-            group = group.selectAll("circle")
-                .data(args.data)
-            
-            //new
-            group.enter().append("circle")
-                .attr("cx",function(d, i){ if(i%x_size === 0){ if(g_width === 0 && align === Dlite.align.HORIZONTAL){g_width = cx;} cx = 0; } cx += (INC + SPACE); return cx;})
-                .attr("cy",function(d, i){ if(i%x_size === 0){ cy += (INC + SPACE); } if(align === Dlite.align.VERTICAL){ g_height = cy;} return cy; })
-                .attr("stroke", function(d){if(q || d === 1){ return args.color; }else{return '#000';}})
-                .attr("fill", function(d){return d === 1 ? args.color :'none'})
-                .attr("r", RADIUS)
-            //update
-            
-            group.attr("stroke",function(d){if(q || d === 1){ return args.color; }else{return '#000';}})
-                .attr("fill",function(d){return d === 1 ? args.color :"none"});
-            
-            //remove
-            group.exit().remove();
-            //return new position coordinates for the next drawing
-            return {
-                x:pos.x + g_width,
-                y:pos.y + g_height
-            }
-    },
-    
-    setupViewer: function(container){
-        var cObject = document.getElementById(container),
-        style = window.getComputedStyle(cObject,null),
-        canvas_width = style.getPropertyValue("width"),
-        canvas_height = style.getPropertyValue("height"),
-        svg = d3.select('#'+container).append("svg")
-            .attr('width',canvas_width)
-            .attr('height',canvas_height)
-            .append("g")
-            .attr("transform","translate(0,0)");
-        
-        return {
-            c:svg,
-            w:canvas_width,
-            h:canvas_height
-        };
     }
 };
